54fa5999b9b8a73dccdd392469c5171ddde6f238
Sparsh Patel <sparsh.patel@kcl.ac.uk>
2022-10-07 16:21:16 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in range(len(items)):
+        if (str(items[i])) in frequencies:
+            frequencies[str(items[i])] = frequencies.get(str(items[i])) + 1
+        else:
+            frequencies[str(items[i])] = 0
     return frequencies
